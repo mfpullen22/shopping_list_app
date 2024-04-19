31dd52c2d003dc899c20ecb0bf4084d3129932c6
Matthew Pullen <matthew.pullen@gmail.com>
2024-04-18 22:36:50 -0500

    Finished demo app

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,14 +19,10 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = "";
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
-  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      setState(() {
-        _isSending = true;
-      });
       final url = Uri.https(
           "flutter-practice-a3a20-default-rtdb.firebaseio.com",
           "shopping-list.json");
@@ -146,22 +142,14 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: _isSending
-                        ? null
-                        : () {
-                            _formKey.currentState!.reset();
-                          },
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
                     child: const Text("Reset"),
                   ),
                   ElevatedButton(
-                    onPressed: _isSending ? null : _saveItem,
-                    child: _isSending
-                        ? const SizedBox(
-                            height: 16,
-                            width: 16,
-                            child: CircularProgressIndicator(),
-                          )
-                        : const Text("Add Item"),
+                    onPressed: _saveItem,
+                    child: const Text("Add Item"),
                   ),
                 ],
               ),
